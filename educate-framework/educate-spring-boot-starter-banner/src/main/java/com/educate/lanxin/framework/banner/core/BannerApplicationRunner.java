b81b53b69f314d7028cdc47e6cb63bae6b0b6b9f
j-sentinel <2724650486@qq.com>
2023-05-02 23:47:08 +0800

    educate-framework写出markdown来确定未来的目标，微调整pom.xm，新增educate-spring-boot-starter-banner模块，明天写更详细的业务

---
+++ educate-framework/educate-spring-boot-starter-banner/src/main/java/com/educate/lanxin/framework/banner/core/BannerApplicationRunner.java
@@ -0,0 +1,18 @@
+package com.educate.lanxin.framework.banner.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+/**
+ * @Author: j-sentinel
+ * @Date: 2023/5/2 23:37
+ */
+@Slf4j
+public class BannerApplicationRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
