caa3ab6b9fdc62c1de78e546a11a4a7c8138024f
j-sentinel <2724650486@qq.com>
2023-05-29 21:18:04 +0800

    手机获取验证码的逻辑大致producer流程写完

--- educate-module-system/educate-module-system-biz/src/main/java/cn/iocoder/educate/module/system/service/sms/SmsSendServiceImpl.java
+++ educate-module-system/educate-module-system-biz/src/main/java/cn/iocoder/educate/module/system/service/sms/SmsSendServiceImpl.java
@@ -1,6 +1,13 @@
 package cn.iocoder.educate.module.system.service.sms;
 
+import cn.iocoder.educate.framework.common.exception.util.ServiceExceptionUtil;
+import cn.iocoder.educate.module.system.dal.dataobject.sms.SmsChannelDO;
+import cn.iocoder.educate.module.system.dal.dataobject.sms.SmsTemplateDO;
+import cn.iocoder.educate.module.system.enums.ErrorCodeConstants;
+import cn.iocoder.educate.module.system.mq.message.sms.SmsSendMessage;
+import cn.iocoder.educate.module.system.mq.producer.sms.SmsProducer;
 import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
 import java.util.Map;
 
 /**
@@ -12,8 +19,53 @@ import java.util.Map;
 @Service
 public class SmsSendServiceImpl implements SmsSendService {
 
+    @Resource
+    private SmsProducer smsProducer;
+
+    @Resource
+    private SmsTemplateService smsTemplateService;
+
+    @Resource
+    private SmsChannelService smsChannelService;
+
     @Override
     public Long sendSingleSms(String mobile, Long userId, Integer userType, String templateCode, Map<String, Object> templateParams) {
+        // 校验短信模板是否合法
+        SmsTemplateDO template = validateSmsTemplate(templateCode);
+        // 校验短信渠道是否合法
+        validateSmsChannel(template.getChannelId());
+        //smsProducer.sendSmsMessage();
+        return null;
+    }
+
+
+
+    /**
+     * 注：此方法只给consumers调用
+     * @param message
+     */
+    @Override
+    public void doSendSms(SmsSendMessage message) {
+
+    }
+
+    /**
+     * 校验短信模板是否合法
+     * @param templateCode
+     * @return
+     */
+    private SmsTemplateDO validateSmsTemplate(String templateCode) {
+        // 获得短信模板。考虑到效率，从缓存中获取，防止缓存穿透
+        SmsTemplateDO templateDO = smsTemplateService.getSmsTemplateByCodeFromCache(templateCode);
+        if(templateDO == null){
+            throw ServiceExceptionUtil.exception(ErrorCodeConstants.SMS_SEND_TEMPLATE_NOT_EXISTS);
+        }
+        return templateDO;
+    }
+
+    private SmsChannelDO validateSmsChannel(Long channelId) {
+        // 获得短信模板。考虑到效率，从缓存中获取
+        smsChannelService.getSmsChannel(channelId);
         return null;
     }
 }
