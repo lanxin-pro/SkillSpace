1db8efc4b9cd050c8dc6c8846da8e3cb15c1bdf5
j-sentinel <2724650486@qq.com>
2023-11-12 16:20:25 +0800

    新增：uniapp端

---
+++ educate-ui-mail-uniapp/pages/users/privacy/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>隐私协议</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
