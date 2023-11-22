e1ef719b0be1ae054f8d42875494cc9df9531620
j-sentinel <2724650486@qq.com>
2023-11-22 21:01:29 +0800

    前端：会员等级页面 优化：对会员分组页面备注的优化

--- educate-ui-admin/src/views/member/level/index.vue
+++ educate-ui-admin/src/views/member/level/index.vue
@@ -2,17 +2,16 @@
   <div class="app-container">
     <!-- 搜索工作栏 -->
     <el-form
-        class="-mb-10px"
         :model="queryParams"
         ref="queryFormRef"
         :inline="true"
         label-width="68px"
         size="small"
     >
-      <el-form-item label="分组名称" prop="name">
+      <el-form-item label="等级名称" prop="name">
         <el-input
             v-model="queryParams.name"
-            placeholder="请输入分组名称"
+            placeholder="请输入等级名称"
             clearable
             @keyup.enter="handleQuery"
             class="!w-240px"
@@ -28,29 +27,18 @@
           />
         </el-select>
       </el-form-item>
-      <el-form-item label="创建时间" prop="createTime">
-        <el-date-picker
-            v-model="queryParams.createTime"
-            value-format="YYYY-MM-DD HH:mm:ss"
-            type="daterange"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-            :default-time="[new Date('1 00:00:00'), new Date('1 23:59:59')]"
-            class="!w-240px"
-        />
-      </el-form-item>
     </el-form>
 
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
-        <el-button size="small" @click="handleQuery" icon="search">搜索</el-button>
-        <el-button size="small" @click="resetQuery" icon="refresh">重置</el-button>
+        <el-button @click="handleQuery" size="small" icon="Search">搜索</el-button>
+        <el-button @click="resetQuery" size="small" icon="Refresh">重置</el-button>
         <el-button
-            size="small"
             type="primary"
+            size="small"
             @click="openForm('create')"
-            icon="plus"
-            v-hasPermi="['member:group:create']"
+            icon="Plus"
+            v-hasPermi="['member:level:create']"
         >
           新增
         </el-button>
@@ -60,11 +48,31 @@
     <!-- 列表 -->
     <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
       <el-table-column label="编号" align="center" prop="id" min-width="60" />
-      <el-table-column label="名称" align="center" prop="name" min-width="80" />
-      <el-table-column label="备注" :show-overflow-tooltip="true" align="center" prop="remark" min-width="100" />
+      <el-table-column label="等级图标" align="center" prop="icon" min-width="80">
+        <template #default="scope">
+          <el-image
+              :src="scope.row.icon"
+              class="h-30px w-30px"
+              :preview-src-list="[scope.row.icon]"
+          />
+        </template>
+      </el-table-column>
+      <el-table-column label="等级背景图" align="center" prop="backgroundUrl" min-width="100">
+        <template #default="scope">
+          <el-image
+              :src="scope.row.backgroundUrl"
+              class="h-30px w-30px"
+              :preview-src-list="[scope.row.backgroundUrl]"
+          />
+        </template>
+      </el-table-column>
+      <el-table-column label="等级名称" align="center" prop="name" min-width="100" />
+      <el-table-column label="等级" align="center" prop="level" min-width="60" />
+      <el-table-column label="升级经验" align="center" prop="experience" min-width="80" />
+      <el-table-column label="享受折扣(%)" align="center" prop="discountPercent" min-width="110" />
       <el-table-column label="状态" align="center" prop="status" min-width="70">
         <template #default="scope">
-          <DictTag :type="DICT_TYPE.COMMON_STATUS" :value="scope.row.status" />
+          <dict-tag :type="DICT_TYPE.COMMON_STATUS" :value="scope.row.status" />
         </template>
       </el-table-column>
       <el-table-column
@@ -74,14 +82,13 @@
           :formatter="dateFormatter"
           min-width="170"
       />
-      <el-table-column label="操作" align="center" width="150px">
+      <el-table-column label="操作" align="center" min-width="110px" fixed="right">
         <template #default="scope">
           <el-button
               link
               type="primary"
               @click="openForm('update', scope.row.id)"
-              v-hasPermi="['member:group:update']"
-              icon="Edit"
+              v-hasPermi="['member:level:update']"
           >
             编辑
           </el-button>
@@ -89,8 +96,7 @@
               link
               type="danger"
               @click="handleDelete(scope.row.id)"
-              v-hasPermi="['member:group:delete']"
-              icon="Delete"
+              v-hasPermi="['member:level:delete']"
           >
             删除
           </el-button>
@@ -107,47 +113,42 @@
     />
 
     <!-- 表单弹窗：添加/修改 -->
-    <GroupForm ref="formRef" @success="getList" />
-
+    <LevelForm ref="formRef" @success="getList" />
   </div>
 </template>
 
 <script setup>
-import { getGroupPage,deleteGroup } from '@/api/member/group/index.js'
+import { deleteLevel, getLevelPage } from '@/api/member/level/index.js'
 import Pagination from '@/components/Pagination/index.vue'
 import { dateFormatter,formatDate } from '@/utils/formatTime'
 import ELComponent from '@/plugins/modal.js'
 import { ref, reactive,onMounted } from 'vue'
-import GroupForm from './GroupForm.vue'
+import LevelForm from './LevelForm.vue'
 import { DICT_TYPE, getIntDictOptions } from '@/utils/dict.js'
 import DictTag from '@/components/DictTag/index.vue'
+
+// 列表的加载中
+const loading = ref(true)
+// 列表的数据
+const list = ref([])
+const queryParams = reactive({
+  name: null,
+  status: null
+})
+const total = ref(0)
+// 搜索的表单
+const queryFormRef = ref()
 
 /** 初始化 **/
 onMounted(() => {
   getList()
 })
 
-// 列表的加载中
-const loading = ref(true)
-// 列表的总页数
-const total = ref(0)
-// 列表的数据
-const list = ref([])
-const queryParams = reactive({
-  pageNo: 1,
-  pageSize: 10,
-  name: null,
-  status: null,
-  createTime: []
-})
-// 搜索的表单
-const queryFormRef = ref()
-
 /** 查询列表 */
 const getList = async () => {
   loading.value = true
   try {
-    const response = await getGroupPage(queryParams)
+    const response = await getLevelPage(queryParams)
     list.value = response.data.list
     total.value = response.data.total
   } finally {
@@ -157,7 +158,6 @@ const getList = async () => {
 
 /** 搜索按钮操作 */
 const handleQuery = () => {
-  queryParams.pageNo = 1
   getList()
 }
 
@@ -179,12 +179,13 @@ const handleDelete = async (id) => {
     // 删除的二次确认
     await ELComponent.confirm('您确定要删除吗？')
     // 发起删除
-    await deleteGroup(id)
+    await deleteLevel(id)
     ELComponent.msgSuccess('删除成功！')
     // 刷新列表
     await getList()
   } catch {}
 }
+
 </script>
 
 <style scoped>
