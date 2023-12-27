1b0fedb74d69f9f66f4a9f24ad585f13317333a6
j-sentinel <2724650486@qq.com>
2023-12-27 23:32:43 +0800

    新增：新增uv-xxx模块组件，购物车UI划分

---
+++ educate-ui-mail-uniapp-vite3/pages/member_purchases/shopping-trolley/components/detail-tabbar.vue
@@ -0,0 +1,83 @@
+<template>
+  <view class="tabbar-box-fixed">
+    <view class="tabbar-box">
+      <view class="footer-left">
+        <uv-checkbox-group @change="onSelectAll" v-model="tempOnSelectAll">
+          <uv-checkbox name="全选" shape="circle" activeColor="#f55b91" label="全选"></uv-checkbox>
+        </uv-checkbox-group>
+      </view>
+      <view class="footer-right">
+        <view class="summation">
+          <text style="margin-right: -10rpx;font-size: 26rpx;">合计：</text>
+          <cn-money :money="16.35" :size="28" color="#ee719b" />
+        </view>
+        <view class="bottom-add">
+          <view>
+            结算(0)
+          </view>
+        </view>
+      </view>
+    </view>
+
+  </view>
+</template>
+
+<script setup>
+import CnMoney from '@/sheep/components/cn-money/cn-money.vue'
+import { ref, reactive, computed } from 'vue'
+
+const tempOnSelectAll = ref(true)
+const state = reactive({
+  editMode: false,
+  selectedList: [],
+});
+// 全选
+const onSelectAll = (check) => {
+  console.log(check)
+}
+</script>
+
+<style scoped lang="scss">
+.tabbar-box-fixed {
+  box-shadow: 2 rpx 2 rpx 2 rpx 2 rpx #ececec;
+  border: 1 rpx solid #ececec;
+  background: #ffffff;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  z-index: 999;
+  .tabbar-box {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20rpx 20rpx;
+    .footer-left {
+      display: flex;
+      align-items: center;
+    }
+    .footer-right {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .summation {
+        margin-right: 20rpx;
+
+      }
+      .bottom-add {
+        /* 不知道是不是错觉的问题，总感觉两个不一样大 */
+        width: 180rpx;
+        height: 72rpx;
+        border-radius: 35rpx;
+        /* 高级的颜色 */
+        /*background: linear-gradient(to right, #aaf, #001);*/
+        background: linear-gradient(to right, #fc7296, #e54c72);
+        color: #ffffff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+
+}
+</style>
--- educate-ui-mail-uniapp-vite3/pages/member_purchases/shopping-trolley/index.vue
+++ educate-ui-mail-uniapp-vite3/pages/member_purchases/shopping-trolley/index.vue
@@ -1,13 +1,40 @@
 <template>
+  <view>
+    <view class="box-nav-bar">
+      <uni-nav-bar
+          background-color="#ffffff"
+          left-icon="left"
+          right-icon="cart"
+          :title="'购物车(' + number + ')'"
+          size="20"
+          :border="false"
+      >
+        <template v-slot:left>
+          <text class="iconfont icon-xiangzuo"></text>
+        </template>
+        <template v-slot:right>
+          <view>管理</view>
+        </template>
+      </uni-nav-bar>
+    </view>
 
+    <view>
+      内容区域
+    </view>
+
+    <view>
+      <DetailTabbar />
+    </view>
+  </view>
 </template>
 
-<script>
-export default {
-  name: "index"
-}
+<script setup>
+import { ref } from "vue"
+import DetailTabbar from './components/detail-tabbar.vue'
+
+const number = ref(3)
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
