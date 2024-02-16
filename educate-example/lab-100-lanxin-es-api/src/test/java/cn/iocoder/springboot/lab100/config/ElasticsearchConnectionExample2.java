63003e811f1bfac7334cc5db55ef9f7cb7a09318
j-sentinel <2724650486@qq.com>
2024-02-16 15:46:23 +0800

    升级：升级示例模块中elasticsearch版本位：8.11.4 新增：测试模组成功

--- educate-example/lab-100-lanxin-es-api/src/test/java/cn/iocoder/springboot/lab100/config/ElasticsearchConnectionExample2.java
+++ educate-example/lab-100-lanxin-es-api/src/test/java/cn/iocoder/springboot/lab100/config/ElasticsearchConnectionExample2.java
@@ -1,50 +1,59 @@
 package cn.iocoder.springboot.lab100.config;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.CreateRequest;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Base64;
 
-/**
- * @author j-sentinel
- * @date 2024/2/15 14:29
- */
-@Configuration
-public class ElasticSearchClientConfig {
+public class ElasticsearchConnectionExample2 {
 
-    @Bean
-    public ElasticsearchClient restHighLevelClient(){
+    @Test
+    public void a() throws IOException {
         // 指定Elasticsearch集群的主机和端口
-        String hostname = "120.46.160.55";
+        String hostname = "http://120.46.160.55:9200";
         int port = 9200;
-
+        
         // 用户名和密码
         String username = "elastic";
         String password = "dp899556677";
         BasicHeader authorization = new BasicHeader("Authorization",
                 "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes())
         );
-        // 创建RestHighLevelClient对象
-        RestClient restClient = RestClient.builder(new HttpHost(hostname, port, "http"))
+        RestClient.builder(new HttpHost(hostname, port, "http"))
                 .setHttpClientConfigCallback(httpClientBuilder ->
                         httpClientBuilder.setDefaultHeaders(Arrays.asList(new Header[]{authorization}))
-                ).build();
+                );
+        // Create the low-level client
+        RestClient restClient = RestClient
+                .builder(HttpHost.create(hostname))
+                .setDefaultHeaders(new Header[]{
+                        new BasicHeader("Authorization",
+                                "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes())
+                        )
+                })
+                .build();
+
         // Create the transport with a Jackson mapper
         ElasticsearchTransport transport = new RestClientTransport(
                 restClient, new JacksonJsonpMapper());
 
         // And create the API client
         ElasticsearchClient esClient = new ElasticsearchClient(transport);
-        return esClient;
+        esClient.indices().create(c -> c
+                .index("test_version_products")
+        );
     }
-
 }
