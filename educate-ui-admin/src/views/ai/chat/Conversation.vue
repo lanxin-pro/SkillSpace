223c30d49e8815ac4502d3a625b892bffdc21999
j-sentinel <2724650486@qq.com>
2024-07-08 15:00:45 +0800

    新增：前端AI对话左侧栏aside的布局提交。准备回滚

--- educate-ui-admin/src/views/ai/chat/Conversation.vue
+++ educate-ui-admin/src/views/ai/chat/Conversation.vue
@@ -11,13 +11,26 @@
       </el-input>
 
       <div class="conversation-list">
-        <el-empty v-if="true" v-loading="true" description=""/>
+        <el-empty v-if="loading" v-loading="loading" description=""/>
         <div v-for="conversationKey in Object.keys(conversationMap)" :key="conversationKey">
+          <!--     时间的展示     -->
           <div
               v-if="conversationMap[conversationKey].length"
               class="conversation-item classify-title"
           >
             <el-text class="mx-1" size="small" tag="b">{{ conversationKey }}</el-text>
+          </div>
+        <!--     每个时间段的数据     -->
+          <div v-for="conversation in conversationMap[conversationKey]"  :key="conversation.id"
+               class="conversation-item">
+            <div :class="
+                conversation.id === activeConversationId ? 'conversation active' : 'conversation'
+              ">
+              <div class="title-wrapper">
+                <img :src="conversation.roleAvatar || roleAvatarDefaultImg" class="avatar" />
+                <span class="title">{{ conversation.title }}</span>
+              </div>
+            </div>
           </div>
 
 
@@ -47,11 +60,13 @@ import { getChatConversationMyList } from '@/api/ai/chat/conversation'
 import { propTypes } from '@/utils/propTypes.js'
 
 // 对话分组 (置顶、今天、三天前、一星期前、一个月前)
-const conversationMap = ref({"a": [1,2,3],"b": [4,5,6],"c": [7,8,9]})
+const conversationMap = ref([])
 // 对话列表
 const conversationList = ref([])
 // 选中的对话，默认为 null
 const activeConversationId = ref(null)
+// 加载中
+const loading = ref(false)
 const props = defineProps({
   activeId: propTypes.string.required
 })
@@ -78,6 +93,8 @@ const getChatConversationList = async () => {
     conversationMap.value = {}
     return
   }
+  // 5. 对话根据时间分组(置顶、今天、一天前、三天前、七天前、30天前)
+  conversationMap.value = await conversationTimeGroup(conversationList.value)
 }
 
 onMounted(async () => {
@@ -85,8 +102,54 @@ onMounted(async () => {
   await getChatConversationList()
 })
 
-// ============ 组件 onMounted
+const conversationTimeGroup = async (list) => {
+    // 排序、指定、时间分组(今天、一天前、三天前、七天前、30天前)
+  const groupMap = {
+    '置顶': [],
+    '今天': [],
+    '一天前': [],
+    '三天前': [],
+    '七天前': [],
+    '三十天前': []
+  }
+  // 当前时间的时间戳
+  const now = Date.now()
+  // 定义时间间隔常量（单位：毫秒）
+  const oneDay = 24 * 60 * 60 * 1000
+  const threeDays = 3 * oneDay
+  const sevenDays = 7 * oneDay
+  const thirtyDays = 30 * oneDay
 
+  for (const conversation of list) {
+    console.log(
+        conversation.updateTime
+    )
+    // 置顶
+    if (conversation.pinned) {
+      groupMap['置顶'].push(conversation)
+      continue
+    }
+    // 计算时间差（单位：毫秒）
+    const diff = now - conversation.updateTime
+    console.log('不同', diff)
+    // 根据时间间隔判断
+    if (diff < oneDay) {
+      groupMap['今天'].push(conversation)
+    } else if (diff < threeDays) {
+      groupMap['一天前'].push(conversation)
+    } else if (diff < sevenDays) {
+      groupMap['三天前'].push(conversation)
+    } else if (diff < thirtyDays) {
+      groupMap['七天前'].push(conversation)
+    } else {
+      groupMap['三十天前'].push(conversation)
+    }
+  }
+  console.log('----groupMap', groupMap)
+  return groupMap
+}
+
+// ============ 组件 onMounted
 const { activeId } = toRefs(props)
 </script>
 
@@ -98,19 +161,22 @@ const { activeId } = toRefs(props)
   justify-content: space-between;
   padding: 0 10px;
   padding-top: 10px;
+  .btn-new-conversation {
+    padding: 18px 0;
+    margin-bottom: 20px;
+  }
+  .conversation-list {
+    .conversation-item {
+      margin-top: 5px;
+    }
+    .classify-title {
+      padding-top: 10px;
+    }
+  }
 }
-.btn-new-conversation {
-  padding: 18px 0;
-  margin-bottom: 20px;
-}
-.conversation-list {
 
-}
 
-.conversation-item {
-  margin-top: 5px;
-}
-.classify-title {
-  padding-top: 10px;
-}
+
+
+
 </style>
